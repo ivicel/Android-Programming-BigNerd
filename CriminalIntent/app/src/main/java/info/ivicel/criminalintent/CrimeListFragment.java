c721b3dcdd8d6b098b77d8cbf9cda085f58a375e
ivicel <31336040+ivicel@users.noreply.github.com>
2017-09-11 22:52:07 +0800

    chapter 09

--- CriminalIntent/app/src/main/java/info/ivicel/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/info/ivicel/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,13 +49,15 @@ public class CrimeListFragment extends Fragment {
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private Crime mCrime;
+        private ImageView mSolvedImageView;
     
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
     
             mTitleTextView = (TextView)itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView)itemView.findViewById(R.id.crime_date);
-            
+            mSolvedImageView = (ImageView)itemView.findViewById(R.id.crime_solved);
+    
             itemView.setOnClickListener(this);
         }
     
@@ -67,6 +70,7 @@ public class CrimeListFragment extends Fragment {
             mCrime = crime;
             mTitleTextView.setText(crime.getTitle());
             mDateTextView.setText(crime.getDate().toString());
+            mSolvedImageView.setVisibility(mCrime.isSolved() ? View.VISIBLE : View.GONE);
         }
     }
     
